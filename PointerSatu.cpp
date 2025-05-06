5bdd67819a6248e68238c89d6eddf5d6d0bf9617
FarabyMalau <farabymuzakki49@gmail.com>
2025-05-06 15:23:08 +0600

    Menggunakan metode objek reference

--- PointerSatu.cpp
+++ PointerSatu.cpp
@@ -11,6 +11,11 @@ class mahasiswa{
 int main (){
     mahasiswa mhs{1}; // Object mhs
     mhs.showNim(); // Member Access Operator
+    
+    mahasiswa &refMhs = mhs; //Pointer Reference refMhs
+    refMhs.nim + 2; // Member Acces Operator
+    mhs.showNim();
     return 0;
+
   
 }
