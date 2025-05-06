9f0ba0b5efbaed78c75ffd8ac67d69a8bb1c18e7
FarabyMalau <farabymuzakki49@gmail.com>
2025-05-06 14:58:18 +0600

    Membuat class lingkaran

--- PointerdanFriendClass.cpp
+++ PointerdanFriendClass.cpp
@@ -15,4 +15,30 @@ public:
         cout << "Masukkan lebar : ";
         cin >> lebar;
     }
-}
+
+    int luasPp(int a, int b)
+    {
+        return a * b;
+    }
+
+    Lingkaran ol;
+
+};
+
+class Lingkaran 
+{
+public:
+    int jarijari;
+
+    void input()
+    {
+        cout << "Masukkan jari-jari : ";
+        cin >> jarijari;
+    }
+
+    int luasLingkaran(int r)
+    {
+        return 3.14 * r * r;
+
+    }
+};
