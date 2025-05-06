c8cfcf03f93158533ea4d9f50ec98a923fd80d50
FarabyMalau <farabymuzakki49@gmail.com>
2025-05-06 15:42:19 +0600

    membuat input data pada class siswa

--- friendClassSatu.cpp
+++ friendClassSatu.cpp
@@ -13,3 +13,31 @@ public:
     friend class siswa;
 };
 
+class siswa
+{
+private:
+    int id;
+
+public:
+    void setId(int pId);
+    void displayAll(orang &a);
+};
+
+void siswa::displayAll(orang &a)
+{
+    cout << id << endl << a.nama;
+}
+void orang::setNama(string pNama)
+{
+    nama = pNama;
+}
+void siswa::setId(int pId)
+{
+    id = pId;
+}
+int main()
+{
+    orang joko;
+    joko.setNama("Joko Susilo")
+    siswa joko_siswa;
+};
